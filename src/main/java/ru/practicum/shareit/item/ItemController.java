ef2a903e632fbcf8a9005a143db0e2b7a4b9ded0
Alla <alla.slyu@yandex.ru>
2023-05-20 18:33:56 +0300

    review - 2

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.dto.ItemMapper;
 import ru.practicum.shareit.item.dto.NewItem;
 
 import java.util.List;
