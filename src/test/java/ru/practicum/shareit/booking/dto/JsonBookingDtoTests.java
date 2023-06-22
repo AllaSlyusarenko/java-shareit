7d1cc92f1618923c715b4cf66c1435b69b5b5ea2
Alla <alla.slyu@yandex.ru>
2023-06-22 15:27:52 +0300

    checkstyle - 4

--- src/test/java/ru/practicum/shareit/booking/dto/JsonBookingDtoTests.java
+++ src/test/java/ru/practicum/shareit/booking/dto/JsonBookingDtoTests.java
@@ -5,13 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonContent;
-import org.springframework.boot.test.autoconfigure.json.JsonTest;
-
 import java.io.IOException;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
-
 @JsonTest
 class JsonBookingDtoTests {
     @Autowired
