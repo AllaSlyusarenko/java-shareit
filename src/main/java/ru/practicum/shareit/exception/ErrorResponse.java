5702c707fd1af1f354a866ed1642b55dc8010fdb
Alla <alla.slyu@yandex.ru>
2023-06-22 22:56:40 +0300

    checkstyle - 6

--- src/main/java/ru/practicum/shareit/exception/ErrorResponse.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorResponse.java
@@ -2,14 +2,10 @@ package ru.practicum.shareit.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import ru.practicum.shareit.Generated;
 
 @Generated
 @Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class ErrorResponse {
     String error;
