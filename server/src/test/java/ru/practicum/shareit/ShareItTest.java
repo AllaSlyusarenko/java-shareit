9ee6cfc0653528818147bdfa2bc1f6993eacd397
Alla <alla.slyu@yandex.ru>
2023-06-30 12:58:56 +0300

    в пропертис добавлены профили для теста, изменения doker-compose

--- server/src/test/java/ru/practicum/shareit/ShareItTest.java
+++ server/src/test/java/ru/practicum/shareit/ShareItTest.java
@@ -3,11 +3,11 @@ package ru.practicum.shareit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class ShareItServerTest {
+class ShareItTest {
 
     @Test
     void main() {
-        Assertions.assertDoesNotThrow(ShareItServerTest::new);
+        Assertions.assertDoesNotThrow(ShareItServer::new);
         Assertions.assertDoesNotThrow(() -> ShareItServer.main(new String[]{}));
     }
 }
